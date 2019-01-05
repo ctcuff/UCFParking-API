d23ff3e47a50f815789811948323dc65c1c93fe2
Cameron Cuff <dev.ctcuff@gmail.com>
2019-01-05 16:46:17 -0500

    Date picker can no longer choose a date in the future

--- static/nav-controller.js
+++ static/nav-controller.js
@@ -1,13 +1,15 @@
 (function () {
   const $navToday = $('#nav-today');
+  const $navWeek = $('#nav-current-week');
   const $navMonth = $('#nav-current-month');
   const $navAll = $('#nav-all');
   const $inputDate = $('#date-picker');
   const $btnSubmit = $('#btn-submit');
-  const month = new Date().getMonth() + 1;
-  const API_ALL = '/data/all';
+  const today = new Date();
   const API_TODAY = '/data/today';
-  const API_CURRENT_MONTH = `/data/month/${month}`;
+  const API_WEEK = '/data/week';
+  const API_CURRENT_MONTH = `/data/month/${today.getMonth() + 1}`;
+  const API_ALL = '/data/all';
 
   $navToday.click(function () {
     // Don't reload the chart if this nav section
@@ -18,9 +20,27 @@
     $(this).addClass('active');
     $navMonth.removeClass('active');
     $navAll.removeClass('active');
+    $navWeek.removeClass('active');
 
     window.chart.destroy();
+    // Makes sure the date picker's value resets to the current
+    // day when it's not selected
+    $inputDate.attr({ placeholder: today.toLocaleDateString() });
     initLineChart(API_TODAY);
+  });
+
+  $navWeek.click(function () {
+    if ($(this).hasClass('active'))
+      return;
+
+    $(this).addClass('active');
+    $navToday.removeClass('active');
+    $navMonth.removeClass('active');
+    $navAll.removeClass('active');
+
+    window.chart.destroy();
+    $inputDate.attr({ placeholder: today.toLocaleDateString() });
+    initLineChart(API_WEEK);
   });
 
   $navMonth.click(function () {
@@ -30,8 +50,10 @@
     $(this).addClass('active');
     $navAll.removeClass('active');
     $navToday.removeClass('active');
+    $navWeek.removeClass('active');
 
     window.chart.destroy();
+    $inputDate.attr({ placeholder: today.toLocaleDateString() });
     initLineChart(API_CURRENT_MONTH);
   });
 
@@ -42,44 +64,39 @@
     $(this).addClass('active');
     $navToday.removeClass('active');
     $navMonth.removeClass('active');
+    $navWeek.removeClass('active');
 
     window.chart.destroy();
+    $inputDate.attr({ placeholder: today.toLocaleDateString() });
     initLineChart(API_ALL);
   });
 
-  $inputDate.attr({ placeholder: new Date().toLocaleDateString() });
+  $inputDate.attr({ placeholder: today.toLocaleDateString() });
   $inputDate.datepicker({
     autoclose: true,
+    todayHighlight: true,
     startDate: '1/2/2019',
-    endDate: '12/31/2019'
-  });
-  // Disable / re-enable the search button when text is typed
-  // into the input
-  $inputDate.on('change paste keyup', function () {
-    if ($(this).val().length > 0) {
-      $btnSubmit.removeAttr('disabled');
-    } else {
-      $btnSubmit.attr({ disabled: 'disabled' });
-    }
+    endDate: today.toLocaleDateString()
   });
 
   $btnSubmit.click(function (event) {
     // Don't let the page refresh
     event.preventDefault();
-    const date = new Date($inputDate.val());
+    $(this).blur();
+    // Prevent submitting an empty date search
+    if ($inputDate.datepicker('getDate') === null)
+      return;
+
+    const date = $inputDate.datepicker('getDate');
     const [month, day] = [date.getMonth() + 1, date.getDate()];
     const url = `/data/month/${month}/day/${day}`;
 
-    // The date was invalid so just return
-    if (!month || !day) {
-      return;
-    }
-
     // Clear the input but set it's selected value as the placeholder
     $inputDate.attr({ placeholder: $inputDate.val() });
-    $inputDate.val('');
+    $inputDate.datepicker('update', '');
 
     $navToday.removeClass('active');
+    $navWeek.removeClass('active');
     $navMonth.removeClass('active');
     $navAll.removeClass('active');
 
