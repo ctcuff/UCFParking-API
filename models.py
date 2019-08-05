99216bae80c6fceed1bd9da541a8fe6415ef09c4
Cameron Cuff <dev.ctcuff@gmail.com>
2019-08-04 20:29:12 -0400

    Updated database models to match MongoDB schema

--- models.py
+++ models.py
@@ -1,13 +1,66 @@
-from flask_sqlalchemy import SQLAlchemy
+import dateutil.parser
+from mongoengine import (
+    StringField,
+    IntField,
+    Document,
+    ListField,
+    LongField,
+    EmbeddedDocument,
+    FloatField,
+    EmbeddedDocumentField,
+    ValidationError
+)
 
-db = SQLAlchemy()
+_GARAGES = [
+    'Garage A',
+    'Garage B',
+    'Garage C',
+    'Garage D',
+    'Garage H',
+    'Garage I',
+    'Garage Libra',
+]
 
 
-class Garage(db.Model):
-    __tablename__ = 'GarageData'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    date = db.Column(db.Text, nullable=False)
-    garage_data = db.Column(db.JSON, nullable=False)
-    month = db.Column(db.Integer, nullable=False)
-    day = db.Column(db.Integer, nullable=False)
-    week = db.Column(db.Integer, nullable=False)
+class GarageEntry(EmbeddedDocument):
+    max_spaces = IntField(required=True, min_value=0)
+    name = StringField(required=True, choices=_GARAGES)
+    percent_full = FloatField(required=True, min_value=0.0, max_value=100.0)
+    spaces_filled = IntField(required=True, min_value=0)
+    spaces_left = IntField(required=True, min_value=0)
+
+
+class Garage(Document):
+    # Specifies the cluster name
+    meta = {'collection': 'garage_data'}
+
+    date = StringField(required=True, unique=True)
+    timestamp = LongField(required=True, unique=True)
+    day = IntField(required=True, min_value=1, max_value=31)
+    week = IntField(required=True, min_value=0, max_value=52)
+    month = IntField(required=True, min_value=1, max_value=12)
+    garage_data = ListField(EmbeddedDocumentField(GarageEntry), required=True)
+
+    def clean(self):
+        garages_used = set(_GARAGES)
+
+        # Make sure the date passed in is valid
+        try:
+            dateutil.parser.parse(self.date)
+        except (TypeError, ValueError, OverflowError) as e:
+            raise ValidationError(f"Couldn't parse date: {e.args[0]}")
+
+        if len(self.garage_data) != 7:
+            raise ValidationError(
+                f'Expected an array of length 7 but got an array of length {len(self.garage_data)}'
+            )
+
+        # Make sure each garage is entered exactly once
+        for entry in self.garage_data:
+            try:
+                garages_used.remove(entry.name)
+            except KeyError:
+                raise ValidationError(f'Invalid garage name: {entry.name}')
+
+        if len(garages_used) != 0:
+            raise ValidationError(f'Entry missing garage(s): {garages_used}')
